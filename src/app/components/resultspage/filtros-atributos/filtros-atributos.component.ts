a4aa67831a0d0faed17756c880eaa1cff85fc400
nanobonilla1909 <mariano.bonilla1909@gmail.com>
2019-01-21 14:25:01 -0300

    Con el Atributo 1 funcionando ok2

--- src/app/components/resultspage/filtros-atributos/filtros-atributos.component.ts
+++ src/app/components/resultspage/filtros-atributos/filtros-atributos.component.ts
@@ -19,14 +19,8 @@ interface AttributesValues{
 
 export class FiltrosAtributosComponent implements OnInit {
 
-
-  selectedCheckBox: boolean[] = [false, false, false, false];
-  opc1:any;
-
-
-// empiezo aca las prubeas
-  selectedCheckBox2: boolean = true; 
-  selectedOptions: number[]=[3, 6];
+  selectedOptions1: number[];
+  selectedOptions2: number[];
 
   @Output()
   selectionChanged: EventEmitter<number[]> = new EventEmitter<number[]>();
@@ -37,72 +31,74 @@ export class FiltrosAtributosComponent implements OnInit {
   @Input()
   product_attributes2: AttributesValues[];
 
-
-  frameworks = ['Angular','React','Sencha'];
   formulario: FormGroup;
  
 
   constructor(private fb: FormBuilder) {
-    
    }
+
 
   ngOnInit() {
 
-    // console.log("PASSSSSSSOOOOOOOO");
-    // console.log(this.product_attributes1);
     this.formulario = this.fb.group({
       precio_desde: [],
       precio_hasta: [],
-      frameworks: this.buildFrameworks(),
-      product_attributes1: this.buildProductAttributes()
+      product_attributes1: this.buildProductAttributes1(),
+      product_attributes2: this.buildProductAttributes2()
     })
   }
 
 
-  buildFrameworks() {
 
-    const values = this.frameworks.map(v => new FormControl(false));
-    return this.fb.array(values);
-  }
+  buildProductAttributes1() {
 
-  buildProductAttributes() {
-
-    
-    // console.log(this.product_attributes1);
     const values = this.product_attributes1.map(v => new FormControl(false));
     return this.fb.array(values);
   }
 
-  get formData() { 
-    
-    return <FormArray>this.formulario.get('frameworks'); 
+  buildProductAttributes2() {
+
+    const values = this.product_attributes2.map(v => new FormControl(false));
+    return this.fb.array(values);
   }
 
-  get formCheckboxes() { 
+
+  get formCheckboxesAttributes1() { 
     
     return <FormArray>this.formulario.get('product_attributes1'); 
+  }
+
+  get formCheckboxesAttributes2() { 
+    
+    return <FormArray>this.formulario.get('product_attributes2'); 
   }
   
 
 
   attributesSelectionChanged(){
- 
 
-    this.selectedOptions = [];
+    this.selectedOptions1 = [];
+    this.selectedOptions2 = [];
     
-   
     console.log("PASSSSSSSAAAAAA");
     var product_attributes1_values = this.formulario.controls.product_attributes1.value
+    var product_attributes2_values = this.formulario.controls.product_attributes2.value
   
     for(var i = 0; i < product_attributes1_values.length; i++) {
        if (product_attributes1_values[i]) {
-          this.selectedOptions.push(this.product_attributes1[i].options_id);
+          this.selectedOptions1.push(this.product_attributes1[i].options_id);
        }
     }  
 
+    for(var i = 0; i < product_attributes2_values.length; i++) {
+      if (product_attributes2_values[i]) {
+         this.selectedOptions2.push(this.product_attributes2[i].options_id);
+      }
+   }  
+
     console.log("ESTE ES EL SELECTED OPTIONS");
-    console.log(this.selectedOptions);
-    this.selectionChanged.emit(this.selectedOptions); 
+    console.log(this.selectedOptions1);
+    this.selectionChanged.emit(this.selectedOptions1); 
   }
 
 }
